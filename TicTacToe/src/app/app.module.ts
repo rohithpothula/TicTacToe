98ff8dccb470d1217ac4ed28f88c4db78cb93f0a
PRohithKumar-Reddy_finastra <PRohithKumar.Reddy@dh.com>
2024-05-22 16:18:38 +0530

    Initial commit

---
+++ TicTacToe/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './board/board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
